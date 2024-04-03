9d8d0a8254f5a47c085972fe0aa6cad8157d079d
ChingLingTung <littlesmall0540@gmail.com>
2024-04-03 19:38:53 +0800

    清單頁新增俄羅斯方塊icon連結、說明頁新增對應的圖片和規則說明卡片及連結

--- next/pages/game/info.js
+++ next/pages/game/info.js
@@ -2,7 +2,7 @@ import { Layout1 } from "@/components/Layout1";
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import Head from "next/head";
-
+import styles from '@/styles/about.module.css'
 import { useRouter } from "next/router";
 
 export default function Info() {
@@ -12,6 +12,29 @@ export default function Info() {
       <Layout1>
         <div className="center">
           <p className="title">About Project</p>
+          <div className={styles.list}>
+          <Link href={"/game/OOXX"}>
+            <div className={styles.card}>
+              <img src='/OX_info.png' alt="OX遊戲"/>
+              <div>
+                <h2>Tic Tac Toe</h2>
+                <p>Two players, take turns putting O or X.</p>
+                <p>The one who first get one row with same mark by up, down, across, or diagonally is the winner.</p>
+              </div>
+            </div>
+          </Link>
+          <Link href={"/game/Tetris_game"}>
+            <div className={styles.card}>
+              <img src='/Tetris_info.png' alt="Tetris"/>
+              <div>
+                <h2>Tetris</h2>
+                <p>Tetrominoes descends automatically, complete lines by moving tetrominoes.</p>
+                <p>The completed lines will disappear and get the points,the game ends when the uncleared lines reach the top of the playing field.</p>
+              </div>
+            </div>
+          </Link>
+            
+          </div>
         </div>
       </Layout1>
       <Head>
