c82d3d4fee20148ae0b2da20a60ffea15718588d
ChingLingTung <littlesmall0540@gmail.com>
2024-04-03 18:13:52 +0800

    在清單頁加上俄羅斯方塊的小icon和連結

--- next/pages/index.js
+++ next/pages/index.js
@@ -12,10 +12,14 @@ export default function Home() {
       <Layout1>
         <div className="center">
           <p className="title">Project List</p>
-          <Link href="/game/OOXX">
+          <div className={styles.list}>
+            <Link href="/game/OOXX">
             <img className={`${styles.game_icon} ${styles.OX_game}`} src="/OX_icon.png" alt="OX遊戲"/>
-          </Link>
-          
+            </Link>
+            <Link href="/game/Tetris_game">
+              <img className={styles.game_icon} src="/Tetris_icon.png" alt="OX遊戲"/>
+            </Link>
+          </div>
         </div>
       </Layout1>
       <Head>
